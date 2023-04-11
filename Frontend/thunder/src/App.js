f26c4ea712a4b1ac0b8cb3902c2c54b4aece55dd
MoustafaAshraf8 <mostafa.ashraf8642@gmail.com>
2023-04-11 07:06:25 +0200

    images added + form basic components

--- Frontend/thunder/src/App.js
+++ Frontend/thunder/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./Custom.css";
 //_____________________________pages______________________________________
 import HomePage from "./Page/HomePage";
 import Product from "./Page/Product";
@@ -8,25 +9,17 @@ import Nav2 from "./components/Nav2";
 import Card from "./components/Card";
 import Slider2 from "./components/Slider2";
 import SideBarTest from "./components/SideBarTest";
+import LoginForm from "./components/LoginForm";
+import LoginForm2 from "./components/LoginForm2";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App d-flex-column justify-content-center vh-100"
+      style={{ border: "solid 3px red" }}
+    >
       <Nav2 />
-      {/* <Sidebar /> */}
-      {/* <CategoriesBar /> */}
-      {/* <Nav2 /> */}
-      {/* <Slider2 />
-      <Card /> */}
-      <div className="d-flex justify-content-left">
-        <SideBarTest />
-        {/* <div>
-          <Slider2 />
-          <Card />
-        </div> */}
-        <Product />
-      </div>
-      ;
+      <LoginForm2 />
     </div>
   );
 }
